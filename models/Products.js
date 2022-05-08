9d2b2c2ded2c665f84852ea8dc498308b18ba8a5
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-05-08 17:10:13 +0530

    Connect MongoDB, created schema for product and ordes

---
+++ models/Products.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+const ProductSchema = new mongoose.schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            maxLength: 60,
+        },
+        desc: {
+            type: String,
+            required: true,
+            maxLength: 200,
+        },
+        img: {
+            type: String,
+            required: true,
+        },
+        prices: {
+            type: [Number],
+            required: true,
+        },
+        extraOptions: {
+            type: [
+                {
+                    text: {
+                        type: String,
+                        required: true,
+                    },
+                    price: {
+                        type: Number,
+                        required: true,
+                    },
+                },
+            ],
+        },
+    },
+    {
+        timeStamps: true,
+    }
+);
+
+export default mongoose.models.Product ||
+    mongoose.model("Product", ProductSchema);
