b4b8bf5de3fbcafa641e1e3da4f57a4c72d2eba7
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-05-21 22:01:07 +0530

    Admin page and delete operation done

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,33 +1,33 @@
 import dbConnect from "../../../util/mango";
 import Product from "../../../models/Product";
 export default async function handler(req, res) {
-    const {method, query: {id}} = req;
+    const {
+        method,
+        query: { id },
+    } = req;
     dbConnect();
-    if (method==="GET") {
+    if (method === "GET") {
         try {
             const products = await Product.findById(id);
             res.status(200).json(products);
-        }
-        catch(err) {
-            res.status.json(err)
-        }
-    }
-    if (method==="PUT") {
-        try {
-            const product = await Product.create(req.body);
-            res.status(201).json(product)
-        }
-        catch (err) {
-            res.status(500).json(err)
+        } catch (err) {
+            res.status.json(err);
         }
     }
-    if (method==="DELETE") {
+    if (method === "PUT") {
         try {
             const product = await Product.create(req.body);
-            res.status(201).json(product)
+            res.status(201).json(product);
+        } catch (err) {
+            res.status(500).json(err);
         }
-        catch (err) {
-            res.status(500).json(err)
+    }
+    if (method === "DELETE") {
+        try {
+            await Product.findByIdAndDelete(id);
+            res.status(200).json("The product has been deleted!");
+        } catch (err) {
+            res.status(500).json(err);
         }
     }
 }
