27f6ea0f0f10a198b0650050eb053c96c760f5b9
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-05-24 23:57:09 +0530

    block request for unauthentic token

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,24 +1,28 @@
 import dbConnect from "../../../util/mango";
 import Product from "../../../models/Product";
 export default async function handler(req, res) {
-    const {method} = req;
+    const { method, cookies } = req;
+
+    const token = cookies.token;
+
     dbConnect();
-    if (method==="GET") {
+    if (method === "GET") {
         try {
             const products = await Product.find();
             res.status(200).json(products);
-        }
-        catch(err) {
-            res.status.json(err)
+        } catch (err) {
+            res.status.json(err);
         }
     }
-    if (method==="POST") {
+    if (method === "POST") {
+        if (!token || token !== process.env.token) {
+            return res.status(401).json("Not authenticated!");
+        }
         try {
             const product = await Product.create(req.body);
-            res.status(201).json(product)
-        }
-        catch (err) {
-            res.status(500).json(err)
+            res.status(201).json(product);
+        } catch (err) {
+            res.status(500).json(err);
         }
     }
 }
